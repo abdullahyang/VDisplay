ba0752ebe621ba7f1efc87e5d465b1592fb65108
ZYHello <1450897626@qq.com>
2021-01-06 22:13:06 +0800

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -98,12 +98,59 @@ namespace VDisplay
         {
 
         }
-
+        
+        float X;
+        float Y;
         private void Form1_Load(object sender, EventArgs e) //装载窗口事件, 是窗体启动时触发的事件，一般写程序你把自己要初始化的东东可以放在Form1_Load中
         {
-
+            this.Resize += new EventHandler(Form1_Resize);//窗体调整大小时引发事件
+            X = this.Width;//获取窗体的宽度
+            Y = this.Height;//获取窗体的高度
+            setTag(this);//调用方法
         }
 
+        private void setTag(Control cons)
+        {
+            //遍历窗体中的控件
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ":" + con.Height + ":" + con.Left + ":" + con.Top + ":" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                    setTag(con);
+            }
+        }
+        
+        private void setControls(float newx, float newy, Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls)
+            {
+                string[] mytag = con.Tag.ToString().Split(new char[] { ':' });//获取控件的Tag属性值，并分割后存储字符串数组
+                float a = Convert.ToSingle(mytag[0]) * newx;//根据窗体缩放比例确定控件的值，宽度
+                con.Width = (int)a;//宽度
+                a = Convert.ToSingle(mytag[1]) * newy;//高度
+                con.Height = (int)(a);
+                a = Convert.ToSingle(mytag[2]) * newx;//左边距离
+                con.Left = (int)(a);
+                a = Convert.ToSingle(mytag[3]) * newy;//上边缘距离
+                con.Top = (int)(a);
+                Single currentSize = Convert.ToSingle(mytag[4]) * newy;//字体大小
+                con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                if (con.Controls.Count > 0)
+                {
+                    setControls(newx, newy, con);
+                }
+            }
+        }
+        
+        void Form1_Resize(object sender, EventArgs e)
+        {
+            float newx = (this.Width) / X; //窗体宽度缩放比例
+            float newy = this.Height / Y;//窗体高度缩放比例
+            setControls(newx, newy, this);//随窗体改变控件大小
+            this.Text = this.Width.ToString() + " " + this.Height.ToString();//窗体标题栏文本
+        }
+        
         private void pictureBox4_Click(object sender, EventArgs e)
         {
 
